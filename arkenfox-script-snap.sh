da7dfda4fe1d1fe574166b3989e369622e43e30a
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2022-12-28 22:02:50 +0000

    added script-cleaner

--- arkenfox-script-snap.sh
+++ arkenfox-script-snap.sh
@@ -72,4 +72,11 @@ zenity --info --text="Please install NoScript and disable WebGL on all levels \n
 
 snap run firefox -P
 
+cd ~
+
+wget https://github.com/trytomakeyouprivate/Arkenfox-softening/raw/main/script-cleaner.sh
+
+chmod +x script-cleaner.sh
+sh script-cleaner.sh
+
 fi
