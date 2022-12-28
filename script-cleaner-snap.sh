25a0748e5864ff6e90a195f40106770c0f335b3b
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2022-12-28 22:50:15 +0000

    Create script-cleaner-snap.sh

---
+++ script-cleaner-snap.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#This script will remove unnessecary "first setup" lines from the arkenfox-softening script
+
+cd ~/.scripts/
+
+# creating the profile folder
+sed -i '2,3d' arkenfox-script-snap.sh
+
+# setting up the profile, starting it, messages, appstarter creation, downloading of this script
+sed -i '54,94d' arkenfox-script-snap.sh
+
+# add new notification
+printf """
+notify-send 'Arkenfox' 'Firefox Arkenfox user.js updated'""" >> arkenfox-script-snap.sh
+
+# remove itself
+rm ~/script-cleaner-snap.sh
