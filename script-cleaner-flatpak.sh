090aad62bad67964598abc67299279abd62a90f5
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2022-12-29 00:55:06 +0000

    Update script-cleaner-flatpak.sh

--- script-cleaner-flatpak.sh
+++ script-cleaner-flatpak.sh
@@ -8,7 +8,7 @@ cd ~/.scripts/
 sed -i '2,3d' arkenfox-script-flatpak.sh
 
 # setting up the profile, starting it, messages, appstarter creation, downloading of this script
-sed -i '54,94d' arkenfox-script-flatpak.sh
+sed -i '53,95d' arkenfox-script-flatpak.sh
 
 # add new notification
 printf """
