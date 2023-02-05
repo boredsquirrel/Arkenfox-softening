2c41f39157213165bcd956305b08789d794c4f84
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2023-02-05 18:56:42 +0100

    Update and rename script-cleaner-native.sh to script-cleaner.sh

--- script-cleaner.sh
+++ script-cleaner.sh
@@ -2,17 +2,17 @@
 
 #This script will remove unnessecary "first setup" lines from the arkenfox-softening script
 
-cd ~/.scripts/
+cd ~/Tools/Scripts
 
 # creating the profile folder
-sed -i '2,3d' arkenfox-script-native.sh
+sed -i '2,5d' arkenfox-script.sh
 
 # setting up the profile, starting it, messages, appstarter creation, downloading of this script
-sed -i '53,95d' arkenfox-script-native.sh
+sed -i '67,102d' arkenfox-script.sh
 
 # add new notification
 printf """
 notify-send 'Arkenfox' 'Firefox Arkenfox user.js updated'""" >> arkenfox-script-native.sh
 
 # remove itself
-rm script-cleaner-native.sh
+rm script-cleaner.sh
