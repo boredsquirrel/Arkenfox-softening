539b26fab5ebbfa1617b9bc8f5d4922d2bb6e3d6
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2023-02-24 20:17:06 +0000

    Update flatpak.sh

--- flatpak.sh
+++ flatpak.sh
@@ -9,7 +9,38 @@ sed -i 's/"~/.mozilla/firefox/"/"~/.var/app/org.mozilla.firefox/.mozilla/firefox
 while true; do
     read -p "Do you want to disable Letterboxing, risking a lack of privacy for convenience? (y/n) " yn
     case $yn in
-        [Yy]* ) sed -i 's/"#sed -i 's/resistFingerprinting.letterboxing"/"sed -i 's/resistFingerprinting.letterboxing"/g' arkenfox-script.sh;;
+        [Yy]* ) printf """
+# Disable Letterboxing ----------------
+sed -i 's/resistFingerprinting.letterboxing", true/resistFingerprinting.letterboxing", false/g' user.js""" >> arkenfox-script.sh && exit;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+while true; do
+    read -p "Do you want to enable Captive Portal functionality, using captive.kuketz.de? (y/n) " yn
+    case $yn in
+        [Yy]* ) printf """
+
+# Allow Captive Portals: --------------
+sed -i 's/user_pref(canonicalURL", ";/canonicalURL", "http://www.captive.kuketz.de/g' user.js
+sed -i 's/portal-service.enabled", false/portal-service.enabled", true/g' user.js
+sed -i 's/network.connectivity-service.enabled", false/network.connectivity-service.enabled", true/g' user.js""" >> arkenfox-script.sh && exit;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+
+
+while true; do
+    read -p "Do you want to enable Pocket news (minimal) (only working using Firefox' startpage) ? (y/n) " yn
+    case $yn in
+        [Yy]* ) printf""" 
+# Enable Pocket news (Start page) ------
+sed -i 's/discoverystreamfeed", false/discoverystreamfeed", true/g' user.js
+sed -i 's/topstories", false/topstories", true/g' user.js
+sed -i 's/highlights.includePocket", false/highlights.includePocket", true/g' user.js""" >> arkenfox-script.sh && exit;;
         [Nn]* ) exit;;
         * ) echo "Please answer yes or no.";;
     esac
