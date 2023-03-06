3a84d08024f9bb56193db64d8b90e745d1e243f2
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2023-03-06 22:38:39 +0000

    Update script-cleaner.sh

--- script-cleaner.sh
+++ script-cleaner.sh
@@ -5,14 +5,10 @@
 cd ~/Tools/Scripts
 
 # creating the profile folder
-sed -i '2,5d' arkenfox-script.sh
+sed '2,7d' arkenfox-script.sh
 
 # setting up the profile, starting it, messages, appstarter creation, downloading of this script
-sed -i '67,102d' arkenfox-script.sh
-
-# add new notification
-printf """
-notify-send 'Arkenfox' 'Firefox Arkenfox user.js updated'""" >> arkenfox-script-native.sh
+sed '53,125d' arkenfox-script.sh
 
 # remove itself
 rm script-cleaner.sh
