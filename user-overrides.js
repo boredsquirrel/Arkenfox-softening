b60ece787613760d1df08d976018e69128ea969d
trytomakeyouprivate <113100745+trytomakeyouprivate@users.noreply.github.com>
2023-10-26 03:17:22 +0200

    Create user-overrides.js

---
+++ user-overrides.js
@@ -0,0 +1,52 @@
+// Arkenfox Override Settings file
+
+// What does this do?
+// https://github.com/arkenfox/user.js/wiki/3.1-Overrides
+// The override changes the stated settings, to fit your needs
+// Please modify it how you like, but don't modify the user.js itself
+
+
+// Cosmetic: prevent Amazon, Google etc. from being pinned to the Newtabpage should you use it
+// remove pinned website ads
+user_pref("browser.newtabpage.pinned","");
+// remove Google & co. from "top sites"
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned","");
+
+
+// delete history after one week
+user_pref("browser.history_expire_days",7);
+// restore pinned tabs
+user_pref("browser.sessionstore.restore_pinned_tabs_on_demand",true);
+
+// Librewolf only, possible through GUI
+//user_pref("identity.fxaccounts.enabled", true);
+
+// Allow search shortcuts (seems to be disabled?)
+user_pref("keyword.enabled", true);
+
+// Allow Firefoxes new Version notifications
+user_pref("browser.startup.homepage_override.mstone", "REPLACEWITHVERSIONNUMBER"); // what to do to remove this line?
+user_pref("browser.startup.homepage", 3);
+
+// History & Session
+// delete history after one week
+user_pref("browser.history_expire_days",7);
+// restore pinned tabs
+user_pref("browser.sessionstore.restore_pinned_tabs_on_demand",true);
+
+user_pref("privacy.sanitize.sanitizeOnShutdown", false);
+user_pref("privacy.clearOnShutdown.cache", false);
+user_pref("privacy.clearOnShutdown.history", false);
+user_pref("privacy.clearOnShutdown.sessions", false);
+
+// remove pinned website ads
+user_pref("browser.newtabpage.pinned","");
+// remove Google & co. from "top sites"
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned","");
+
+// System colors
+user_pref("browser.display.use_system_colors", true);
+
+user_pref("browser.cache.disk.enable", false);
+
+user_pref("webgl.disabled", false);
