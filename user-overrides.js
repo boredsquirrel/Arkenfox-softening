005b66637afd21157a94a4d232b8963009529e2f
boredsquirrel <boredsquirrel@secure.mailbox.org>
2024-07-24 16:59:01 +0200

    Big fixes

--- user-overrides.js
+++ user-overrides.js
@@ -8,13 +8,13 @@
 
 // Cosmetic: prevent Amazon, Google etc. from being pinned to the Newtabpage should you use it
 // remove pinned website ads
-user_pref("browser.newtabpage.pinned","");
+user_pref("browser.newtabpage.pinned", "");
 // remove Google & co. from "top sites"
-user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned","");
+user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned", "");
 
 
 // delete history after one week
-user_pref("browser.history_expire_days",7);
+user_pref("browser.history_expire_days", 7);
 // restore pinned tabs
 user_pref("browser.sessionstore.restore_pinned_tabs_on_demand",true);
 
@@ -28,35 +28,36 @@ user_pref("keyword.enabled", true);
 user_pref("privacy.resistFingerprinting.letterboxing", false);
 
 // Allow Mozilla Addons Manager for regular behavior
-user_pref("privacy.resistFingerprinting.block_mozAddonManager", block);
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", false);
 
 // Allow Firefoxes new Version notifications
-user_pref("browser.startup.homepage_override.mstone", "REPLACEWITHVERSIONNUMBER"); // what to do to remove this line?
-user_pref("browser.startup.homepage", "about:home");
+// user_pref("browser.startup.homepage_override.mstone", "REPLACEWITHVERSIONNUMBER"); // this is removed in arkenfox, and needs to be removed entirely, otherwise you dont get version upgrade infos in the browser.
+//user_pref("browser.startup.homepage", "about:home");
 user_pref("browser.startup.page", 3);
 
 // History & Session
 // delete history after one week
-user_pref("browser.history_expire_days",7);
+user_pref("browser.history_expire_days", 7);
 // restore pinned tabs
 user_pref("browser.sessionstore.restore_pinned_tabs_on_demand",true);
 
-user_pref("privacy.sanitize.sanitizeOnShutdown", false);
-user_pref("privacy.clearOnShutdown.cache", false);
-user_pref("privacy.clearOnShutdown.history", false);
+//user_pref("privacy.sanitize.sanitizeOnShutdown", false); //prevents clearing Cookies on Shutdown
+// user_pref("privacy.clearOnShutdown.cache", false); //clears a lot of trackers like ETags
+user_pref("privacy.clearOnShutdown.history", false); // usability
 user_pref("privacy.clearOnShutdown.sessions", false);
 
 // System colors
 user_pref("browser.display.use_system_colors", true);
 
-user_pref("webgl.disabled", false);
+user_pref("webgl.disabled", false); // use noscript! "WebGL fingerprint defender" may also help
 
 
 // speed
-user_pref("network.http.max-persistent-connections-per-server" 30);
+user_pref("network.http.max-persistent-connections-per-server", 30);
 user_pref("browser.cache.disk.enable", false);
 
-// Language
+// Language CHANGE TO YOUR NATIVE ONE
+// this is just here for usability.
 user_pref("intl.accept_languages", "de-DE, de");
 user_pref("javascript.use_us_english_locale", false); // [HIDDEN PREF]
 
@@ -67,7 +68,7 @@ user_pref("javascript.use_us_english_locale", false); // [HIDDEN PREF]
 
 // enable the native system Portal (Flatpak)
 user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
-user_pref("widget.use-xdg-desktop-portal.location", 1);
-user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
-user_pref("widget.use-xdg-desktop-portal.open-uri", 1);
-user_pref("widget.use-xdg-desktop-portal.settings", 1);
+//user_pref("widget.use-xdg-desktop-portal.location", 1);
+//user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
+//user_pref("widget.use-xdg-desktop-portal.open-uri", 1);
+//user_pref("widget.use-xdg-desktop-portal.settings", 1);
